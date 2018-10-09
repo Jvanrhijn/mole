94dfe7b525e39fe7dd96def082ac634220a5726a
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-10-09 10:26:39 +0200

    Write test for determinant calculation
    Signed-off-by: Jesse van Rhijn <jesse.v.rhijn@gmail.com>

--- src/math/mat_ops.rs
+++ src/math/mat_ops.rs
@@ -3,15 +3,14 @@ use std::result::Result::{Ok, Err};
 // Third pary imports
 use assert;
 use ndarray::{Array2, Array};
-use ndarray_linalg::{qr::{QR, QRSquare, QRSquareInto}, error::LinalgError};
+use ndarray_linalg::{qr, error::LinalgError};
 use ndarray_rand::RandomExt;
 use rand::distributions::Range;
 
 pub fn det_abs<M>(mat: &M) -> Result<f64, LinalgError> 
 where
-    M: QRSquare<R=Array2<f64>>,
+    M: qr::QRSquare<R=Array2<f64>>,
 {
-    //println!("{:?}", mat);
     let (_, r) = mat.qr_square()?;
     Ok(r.diag().iter().fold(1., |acc, x| acc*x).abs())
 }
@@ -19,12 +18,57 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ops::Neg;
+    use rand::{random, distributions::uniform::SampleUniform};
+
+    struct TestData {
+        num_tests: usize
+    }
+
+    impl TestData {
+        pub fn new(num_tests: usize) -> Self {
+            TestData{num_tests}
+        }
+
+        pub fn random_square_matrix<T>(size: usize, range: T) -> Array2<T>
+        where
+            T: SampleUniform + Neg<Output=T> + Copy
+        {
+            Array::random((size, size), Range::new(-range, range))
+        }
+
+        pub fn random_sized_square_matrix<T>(range: T) -> Array2<T>
+        where
+            T: SampleUniform + Neg<Output=T> + Copy
+        {
+            let size = random::<u8>();
+            Self::random_square_matrix(size as usize, range)
+        }
+
+
+    }
+
+    fn setup() -> TestData {
+        TestData::new(10)
+    }
 
     #[test]
-    fn test_det_square() {
-        //let mat: Array2<f64> = array![[1., 2.], [3., 4.]];
-        let mat: Array2<f64> = Array::random((2, 2), Range::new(-1., 1.));
-        let det: f64 = mat[[0, 0]]*mat[[1, 1]] - mat[[0, 1]]*mat[[1, 0]];
-        assert::close(det_abs(&mat).unwrap(), det.abs(), 1e-5);
+    fn test_det_2x2() {
+        for _i in 0..setup().num_tests {
+            let mat: Array2<f64> = TestData::random_square_matrix::<f64>(2, 1.);
+            let det: f64 = mat[[0, 0]]*mat[[1, 1]] - mat[[0, 1]]*mat[[1, 0]];
+            assert::close(det_abs(&mat).unwrap(), det.abs(), 1e-5);
+        }
     }
+
+    #[test]
+    fn test_transpose() {
+        for _i in 0..setup().num_tests {
+            let mat: Array2<f64> = TestData::random_sized_square_matrix(1.);
+            let det = det_abs(&mat).unwrap();
+            let tol = det*1e-5;
+            assert::close(det, det_abs(&mat.t()).unwrap(), tol);
+        }
+    }
+
 }
