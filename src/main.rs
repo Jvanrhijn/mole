ab860b5a74fdc38efc4a19107c208a6e5f10d6ab
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-10-10 17:25:01 +0200

    Add small test to main for two-particle wave function
    Signed-off-by: Jesse van Rhijn <jesse.v.rhijn@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate rand;
 extern crate assert;
 
 use std::vec::Vec;
-use ndarray::Array1;
+use ndarray::{Array1, arr2};
 
 use traits::function::Function;
 
@@ -34,12 +34,15 @@ mod determinant;
 mod basis_sets;
 
 fn main() {
-    let mut basis_set = Vec::<&Fn(&Array1<f64>) -> f64>::new();
-    basis_set.push(&math::basis::hydrogen_1s);
-    basis_set.push(&math::basis::hydrogen_2s);
+    let mut basis_set_first = Vec::<&Fn(&Array1<f64>) -> f64>::new();
+    let mut basis_set_second = Vec::<&Fn(&Array1<f64>) -> f64>::new();
+    basis_set_first.push(&math::basis::hydrogen_1s);
+    basis_set_second.push(&math::basis::hydrogen_2s);
 
-    let orbital = orbitals::OrbitalExact::new(ndarray::Array1::from_vec(vec![1., 1.]), basis_set);
-    let wf = wf::SingleDeterminant::new(vec![orbital]);
-    let cfg = ndarray::Array2::<f64>::ones((2, 3));
+    let orbital1 = orbitals::OrbitalExact::new(array![1.], basis_set_first);
+    let orbital2 = orbitals::OrbitalExact::new(array![1.], basis_set_second);
+
+    let wf = wf::SingleDeterminant::new(vec![orbital1, orbital2]);
+    let cfg = arr2(&[[1., -1., 0.], [-1., 1., 1.]]);
     println!("{}", wf.value(&cfg).unwrap())
 }
