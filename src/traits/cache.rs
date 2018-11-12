df11c480db6afee253ebc3fd4b87fd0e205618a2
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-11-12 12:13:42 +0100

    Add Cache trait
    Signed-off-by: Jesse van Rhijn <jesse.v.rhijn@gmail.com>

---
+++ src/traits/cache.rs
@@ -0,0 +1,17 @@
+pub trait Cache<T> {
+    /// storage type e.g. ndarray::Array2
+    type A;
+    /// value type e.g. (f64, Array2<f64>, f64) (value/grad/laplacian)
+    type V;
+    /// update data e.g. usize moved-electron-index
+    type U;
+
+    /// Refresh the cached data
+    fn refresh(&mut self, new: T);
+
+    /// Update the cache with a new T and any data needed to update the cache
+    fn update(&mut self, ud: U, new: T);
+
+    /// Return the current value of the cached data
+    fn current_value(&self) -> V;
+}
