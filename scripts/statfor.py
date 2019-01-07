3c8ec17c48531b50d0ec17c07b969a162b386b18
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-01-07 17:38:08 +0100

    Get blocking analysis working

--- scripts/statfor.py
+++ scripts/statfor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Read a stream of data from a file, perform a statistical analysis"""
 import sys
 import math
@@ -8,6 +9,10 @@ import matplotlib.pyplot as plt
 MAX_STEPS = 200
 
 
+def int_width(num):
+    """Return decimal with of integer number"""
+    return math.floor(math.log10(num))
+
 def read_data(fpath):
     """Read data from file located at fpath"""
     return np.array(list(float(line) for line in open(fpath, "r")))
@@ -17,12 +22,48 @@ def correlation(data, mean, var):
     """Calculate correlation time, effective N, error"""
     nsteps = len(data)
     tcorr = 1
-    for i in range(1, min(MAX_STEPS, nsteps-1)+1):
-        corr = ((data[:nsteps-i] - mean)*(data[i:] - mean))[:nsteps-i].sum()
-        corr /= (nsteps-i)*var
-        tcorr += 2*corr*(0 if corr < 0 else 1)
-    tcorr = max(1, tcorr)
+    # needed for formatting
+    max_i = min(MAX_STEPS, nsteps -1)
+    max_width = int_width(max_i)
+    f = 1
+    with open("corr.out", "w") as file:
+        for i in range(1, max_i + 1):
+            corr = ((data[:nsteps-i] - mean)*(data[i:] - mean))[:nsteps-i].sum()
+            corr /= (nsteps-i)*var
+            tcorr += 2*corr*(0 if corr < 0 else 1)
+            file.write(f"{i}" +" "*(max_width-int_width(i)+4) + f"{corr:.10e}\n")
+        tcorr = max(1, tcorr)
+
     return tcorr, nsteps/tcorr, math.sqrt(var*tcorr/nsteps)
+
+
+def blocking(data):
+    """Perform blocking analysis"""
+    ndata = len(data)
+    min_left = 20
+    nsizes = 100
+    large = int(ndata/min_left)
+    step_size = int(max(1, large/nsizes))
+
+    # needed for formatting
+    max_width = int_width(large+1)
+    
+    sizes, errors = [], [] 
+    with open("blocking.out", "w") as file:
+        for size in range(1, large+1, step_size):
+            nblocks = int(ndata/size)
+            # block averages
+            blocks = np.array_split(data, nblocks)
+            averages_blk = np.array(list(map(np.mean, blocks)))
+            averages_sq_blk = np.array([ab**2 for ab in averages_blk]).mean()
+            # estimated error of mean at this block size
+            errors.append((np.sqrt((averages_sq_blk - averages_blk.mean()**2)\
+                    /(nblocks-1))))
+            sizes.append(size)
+            file.write(f"{size}" \
+                    +" "*(max_width-int_width(size)+4) + f"{errors[-1]:.10e}\n")
+
+    return np.array(errors), np.array(sizes)
 
 
 if __name__ == "__main__":
@@ -32,6 +73,15 @@ if __name__ == "__main__":
     VAR = SERIES.var(ddof=1)
 
     TCORR, NEFF, SIGMA = correlation(SERIES, MEAN, VAR)
+
+    ERRORS, SIZES = blocking(SERIES)
+    
+    if "-p" in sys.argv:
+        plt.plot(len(SERIES)/SIZES, ERRORS, '.')
+        plt.ylabel("$\sigma$")
+        plt.xlabel("nblk")
+        plt.grid()
+        plt.show()
 
     # Print output
     print(f"average\t\t{MEAN:.10f}")
