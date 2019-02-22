41148dc8ddeb1e030eff3896d326f525212fb6bf
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-02-22 21:48:01 +0100

    Fix some code duplication in basis
    Signed-off-by: Jesse van Rhijn <jesse.v.rhijn@gmail.com>

--- src/basis/src/basis.rs
+++ src/basis/src/basis.rs
@@ -8,6 +8,25 @@ pub struct GaussianBasis {
     widths: Vec<f64>
 }
 
+fn linear_combination_general(pos: &Array1<f64>, coeffs: &Array2<f64>, centers: &Array2<f64>, widths: &Vec<f64>, func: &Fn(&Array1<f64>, f64) -> Vgl) -> Vgl {
+    let mut value = 0.0;
+    let mut grad = Array1::<f64>::zeros(3);
+    let mut laplacian = 0.0;
+    Zip::from(centers.genrows())
+        .and(coeffs.genrows())
+        .apply(|center, coeffs| {
+            let (v, g, l) = coeffs.iter().zip(widths.iter())
+                .fold((0.0, Array1::<f64>::zeros(3), 0.0) , |acc, (&c, &w)| {
+                    let (value, gradient, laplacian) = func(&(pos - &center), w);
+                    (acc.0 + c * value, acc.1 + c * &gradient, acc.2 + c * laplacian)
+                });
+            value += v;
+            grad += &g;
+            laplacian += l;
+        });
+    (value, grad, laplacian)
+}
+
 impl GaussianBasis {
     pub fn new(centers: Array2<f64>, widths: Vec<f64>) -> Self {
         Self{centers, widths}
@@ -16,22 +35,7 @@ impl GaussianBasis {
 
 impl BasisSet for GaussianBasis {
     fn linear_combination(&self, pos: &Array1<f64>, coeffs: &Array2<f64>) -> Vgl {
-        let mut value = 0.0;
-        let mut grad = Array1::<f64>::zeros(3);
-        let mut laplacian = 0.0;
-        Zip::from(self.centers.genrows())
-            .and(coeffs.genrows())
-            .apply(|center, coeffs| {
-                let (v, g, l) = coeffs.iter().zip(self.widths.iter())
-                    .fold((0.0, Array1::<f64>::zeros(3), 0.0) , |acc, (&c, &w)| {
-                        let (value, gradient, laplacian) = gaussian(&(pos - &center), w);
-                        (acc.0 + c * value, acc.1 + c * &gradient, acc.2 + c * laplacian)
-                    });
-                value += v;
-                grad += &g;
-                laplacian += l;
-            });
-            (value, grad, laplacian)
+        linear_combination_general(pos, coeffs, &self.centers, &self.widths, &gaussian)
     }
 }
 
@@ -49,22 +53,7 @@ impl Hydrogen1sBasis {
 
 impl BasisSet for Hydrogen1sBasis {
     fn linear_combination(&self, pos: &Array1<f64>, coeffs: &Array2<f64>) -> Vgl {
-        let mut value = 0.0;
-        let mut grad = Array1::<f64>::zeros(3);
-        let mut laplacian = 0.0;
-        Zip::from(self.centers.genrows())
-            .and(coeffs.genrows())
-            .apply(|center, coeffs| {
-                let (v, g, l) = coeffs.iter().zip(self.widths.iter())
-                    .fold((0.0, Array1::<f64>::zeros(3), 0.0) , |acc, (&c, &w)| {
-                        let (value, gradient, laplacian) = hydrogen_1s(&(pos - &center), w);
-                        (acc.0 + c * value, acc.1 + c * &gradient, acc.2 + c * laplacian)
-                    });
-                value += v;
-                grad += &g;
-                laplacian += l;
-            });
-        (value, grad, laplacian)
+        linear_combination_general(pos, coeffs, &self.centers, &self.widths, &hydrogen_1s)
     }
 }
 
@@ -82,21 +71,6 @@ impl Hydrogen2sBasis {
 
 impl BasisSet for Hydrogen2sBasis {
     fn linear_combination(&self, pos: &Array1<f64>, coeffs: &Array2<f64>) -> Vgl {
-        let mut value = 0.0;
-        let mut grad = Array1::<f64>::zeros(3);
-        let mut laplacian = 0.0;
-        Zip::from(self.centers.genrows())
-            .and(coeffs.genrows())
-            .apply(|center, coeffs| {
-                let (v, g, l) = coeffs.iter().zip(self.widths.iter())
-                    .fold((0.0, Array1::<f64>::zeros(3), 0.0) , |acc, (&c, &w)| {
-                        let (value, gradient, laplacian) = hydrogen_2s(&(pos - &center), w);
-                        (acc.0 + c * value, acc.1 + c * &gradient, acc.2 + c * laplacian)
-                    });
-                value += v;
-                grad += &g;
-                laplacian += l;
-            });
-        (value, grad, laplacian)
+        linear_combination_general(pos, coeffs, &self.centers, &self.widths, &hydrogen_2s)
     }
 }
