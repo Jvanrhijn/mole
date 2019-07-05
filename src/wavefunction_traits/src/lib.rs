6e222b5585c1232b93e06c62e0e47e060e9259c0
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-07-05 21:24:32 +0200

    Have Cache::refresh return Result

--- src/wavefunction_traits/src/lib.rs
+++ src/wavefunction_traits/src/lib.rs
@@ -4,12 +4,14 @@ use ndarray::{Array, Array2};
 type Vgl = (f64, Array2<f64>, f64);
 type Ovgl = (Option<f64>, Option<Array2<f64>>, Option<f64>);
 
+type Result<T> = std::result::Result<T, Error>;
+
 pub trait Cache {
     /// update data e.g. usize moved-electron-index
     type U;
 
     /// Refresh the cached data
-    fn refresh(&mut self, new: &Array2<f64>);
+    fn refresh(&mut self, new: &Array2<f64>) -> Result<()>;
 
     /// Calculate updated value of the cache given update data and new configuration,
     /// and set this data enqueued
@@ -32,16 +34,16 @@ pub trait Cache {
 pub trait Function<T> {
     type D;
 
-    fn value(&self, cfg: &Array<T, Self::D>) -> Result<T, Error>;
+    fn value(&self, cfg: &Array<T, Self::D>) -> Result<T>;
 }
 
 /// Interface for creating once- and twice differentiable functions.
 pub trait Differentiate {
     type D;
 
-    fn gradient(&self, cfg: &Array<f64, Self::D>) -> Result<Array<f64, Self::D>, Error>;
+    fn gradient(&self, cfg: &Array<f64, Self::D>) -> Result<Array<f64, Self::D>>;
 
-    fn laplacian(&self, cfg: &Array<f64, Self::D>) -> Result<f64, Error>;
+    fn laplacian(&self, cfg: &Array<f64, Self::D>) -> Result<f64>;
 }
 
 pub trait WaveFunction {
