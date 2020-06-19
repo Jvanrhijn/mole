6cb04672f6dbcd9ae3887c37228f35fec1fe6769
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2020-06-19 15:18:57 +0200

    Finish rudimentary DMC example

--- examples/dmc.rs
+++ examples/dmc.rs
@@ -1,8 +1,12 @@
 use mole::prelude::*;
-use ndarray::{Array1, Array2, Array, Ix1, Ix2, Axis, array};
+use ndarray::{Array1, Array2, Array, Ix1, Ix2, Axis, array, s};
 use ndarray_linalg::Norm;
 use std::collections::HashMap;
 use rand::{SeedableRng, StdRng};
+
+use rand::distributions::{Normal, Range, Uniform};
+use rand::{FromEntropy, Rng};
+use ndarray_rand::RandomExt;
 
 // DMC test for hydrogen atom,
 // testing ground for library integration of
@@ -95,7 +99,7 @@ static BLOCK_SIZE: usize = 10;
 
 fn main() {
     // Build wave function
-    let ansatz = GaussianWaveFunction::new(0.5);
+    let ansatz = GaussianWaveFunction::new(1.0);
 
     let metrop = MetropolisDiffuse::from_rng(0.1, StdRng::from_seed([0; 32]));
 
@@ -103,7 +107,7 @@ fn main() {
     let hamiltonian = ElectronicHamiltonian::from_ions(array![[0.0, 0.0, 0.0]], array![1]);
 
     let obs = operators! {
-        "Energy" => hamiltonian,
+        "Energy" => hamiltonian.clone(),
         "Parameter gradient" => ParameterGradient,
         "Wavefunction value" => WavefunctionValue
     };
@@ -113,13 +117,81 @@ fn main() {
     // first do a VMC run to obtain a variationally optimized wave function
     let vmc = VmcRunner::new(sampler, SteepestDescent::new(1e-5), Logger);
 
-    let (guiding_wf, energies, errors) = vmc.run_optimization(ITERS, TOTAL_SAMPLES, BLOCK_SIZE, 4)
+    let (mut guiding_wf, energies, errors) = vmc.run_optimization(ITERS, TOTAL_SAMPLES, BLOCK_SIZE, 4)
                                             .expect("VMC run failed");
 
     let energy = energies.iter().last().unwrap();
     let error = errors.iter().last().unwrap();
 
-    println!("\nVMC Energy:     {} +/- {:.*}", energy, 8, error);
+    println!("\nVMC Energy:     {} +/- {:.*}\n", energy, 8, error);
 
     // now do a DMC run
+
+    // first, extract the standard deviation
+    // of the probability density |psi^2|:guiding_wf
+    let alpha = guiding_wf.parameters()[0];
+    let stdev = alpha/2.0;
+
+    // sample a set of starting configurations
+    // from the wave function
+    let num_confs = 4000;
+    const TAU: f64 = 0.05;
+    const DMC_ITERS: usize = 100;
+
+    //let mut confs = Array2::random_using((num_confs, 3), Normal::new(0.0, stdev), &mut StdRng::from_seed([1_u8; 32]));
+    let mut rng = StdRng::from_seed([1_u8; 32]);
+    let mut confs = vec![];
+    for _ in 0..num_confs {
+        confs.push(Array2::random_using((1, 3), Normal::new(0.0, stdev), &mut rng));
+    }
+
+    // initialize trial energy
+    let mut trial_energy = *energy;
+    // initialize dmc energy
+    let mut metrop = MetropolisDiffuse::from_rng(TAU, rng.clone());
+
+    // for the number of dmc iterations
+    for j in 0..DMC_ITERS {
+        let mut energy_acc = 0.0;
+        let mut branch_acc = 0.0;
+
+        // for each configuration
+        for i in 0..num_confs {
+            // propose electron move using metropolis diffusion algorithm
+            let conf = &confs[i];
+            let new_conf = if let Some(x) = metrop.move_state(&mut guiding_wf, &conf, 0).unwrap() {
+                x
+            } else {
+                conf.clone()
+            };
+
+            // skip FN approximation since there are no nodes
+
+            // branching factor
+            let local_e = hamiltonian.act_on(&guiding_wf, &conf).unwrap().get_scalar().unwrap()/guiding_wf.value(&conf).unwrap();
+            let local_e_new = hamiltonian.act_on(&guiding_wf, &new_conf).unwrap().get_scalar().unwrap()/guiding_wf.value(&conf).unwrap();
+        
+            let branch_factor = f64::exp(-TAU * (0.5*(local_e + local_e_new) - trial_energy));
+            branch_acc += branch_factor;
+            energy_acc += local_e_new;
+
+            // clone configuration according to branching algorithm
+            let num_copies = (branch_factor + rng.gen::<f64>()) as usize;
+
+            for _ in 0..num_copies {
+                confs.push(new_conf.clone());
+            }
+        }
+        energy_acc /= branch_acc;
+        // update trial energy
+        trial_energy = energy_acc;
+   
+        // randomly delete a number of walkers
+        let excess = confs.len() - num_confs;
+        for _ in 0..excess {
+            confs.remove(rng.gen_range(0, confs.len()));
+        }
+
+        println!("DMC Energy:   {:.8}", trial_energy);
+    }
 }
