c44302842892d7af9a9ee095102784abb0636613
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2020-07-01 21:40:43 +0200

    Start implementing optimal SR method

--- src/dmc/src/branching.rs
+++ src/dmc/src/branching.rs
@@ -8,6 +8,7 @@ pub struct SRBrancher;
 
 impl SRBrancher {
     pub fn new() -> Self {
+        unimplemented!();
         Self {}
     }
 }
@@ -18,8 +19,7 @@ impl<R: RngCore> BranchingAlgorithm<R> for SRBrancher {
         walkers: &Vec<(f64, Array2<f64>)>,
         rng: &mut R,
     ) -> Vec<(f64, Array2<f64>)> {
-        let global_weight = walkers.iter().fold(0.0, |acc, (w, _)| acc + w);
-        let new_weight = global_weight / walkers.len() as f64;
+        let global_weight = walkers.iter().fold(0.0, |acc, (w, _)| acc + w) / walkers.len() as f64;
         let max_weight = walkers.iter().fold(0.0, |acc, (w, _)| f64::max(acc, *w));
         // normalize weights by the maximum weight
         let norm_factor = walkers.len() as f64 / max_weight;
@@ -34,8 +34,53 @@ impl<R: RngCore> BranchingAlgorithm<R> for SRBrancher {
         // construct new walkers
         let mut new_walkers = vec![];
         for _ in 0..walkers.len() {
-            new_walkers.push((new_weight, wc.sample(rng).clone()));
+            new_walkers.push((global_weight, wc.sample(rng).clone()));
         }
+        new_walkers
+    }
+}
+
+pub struct OptimalSRBrancher;
+
+impl OptimalSRBrancher {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<R: RngCore> BranchingAlgorithm<R> for OptimalSRBrancher {
+    fn branch(
+        &mut self,
+        walkers: &Vec<(f64, Array2<f64>)>,
+        rng: &mut R,
+    ) -> Vec<(f64, Array2<f64>)> {
+        let global_weight = walkers.iter().fold(0.0, |acc, (w, _)| acc + w) / walkers.len() as f64;
+        let max_weight = walkers.iter().fold(0.0, |acc, (w, _)| f64::max(acc, *w));
+
+        // obtain the positive and negative walkers
+        let positive_walkers = walkers.iter().filter(|(w, _)| *w >= 1.0);
+        let negative_walkers = walkers.iter().filter(|(w, _)| *w < 1.0);
+        let num_reconf = positive_walkers.map(|(w, _)| (w/walkers.len() as f64 - 1.0).abs()).sum::<f64>();
+        let num_reconf_minus = negative_walkers.map(|(w, _)| (w/walkers.len() as f64 - 1.0).abs()).sum::<f64>();
+
+
+        // normalize weights by the maximum weight
+        let norm_factor = walkers.len() as f64 / max_weight;
+        let mut confs_weighted: Vec<_> = walkers
+            .iter()
+            .map(|(w, c)| Weighted {
+                weight: (w * norm_factor) as u32,
+                item: c,
+            })
+            .collect();
+        let wc = WeightedChoice::new(&mut confs_weighted);
+
+        // construct new walkers
+        let mut new_walkers = vec![];
+        for _ in 0..walkers.len() {
+            new_walkers.push((global_weight, wc.sample(rng).clone()));
+        }
+
         new_walkers
     }
 }
